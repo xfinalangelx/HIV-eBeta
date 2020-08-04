7b23516211bee7dcb689fb331cd5ee5037c5502e
finalangel <55591218+xfinalangelx@users.noreply.github.com>
2020-08-04 21:41:06 +0800

    Added a login page and finalized GUI for splash

--- app/src/main/java/com/example/hiv_ebeta/MainActivity.java
+++ app/src/main/java/com/example/hiv_ebeta/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.hiv_ebeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.example.hiv_ebeta.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button nextBtn;
     LottieAnimationView lottieAnimationView;
 
     @Override
@@ -17,29 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lottieAnimationView = (LottieAnimationView) findViewById(R.id.lottieAnimationView);
-        //startCheckAnimation();
         lottieAnimationView.setVisibility(View.VISIBLE);
         lottieAnimationView.setAnimation("4096-heal.json");
         lottieAnimationView.loop(true);
         lottieAnimationView.playAnimation();
+
+        nextBtn = (Button) findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
     }
 
-   // private void startCheckAnimation(){
-   //     ValueAnimator animator = ValueAnimator.ofFloat(0f,1f).setDuration(10000);
-   //     animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-   //         @Override
-   //         public void onAnimationUpdate(ValueAnimator animation) {
-   //             lottieAnimationView.setProgress((Float)animation.getAnimatedValue());
-   //             lottieAnimationView.loop(true);
-   //         }
-   //     });
-//
-   //     if(lottieAnimationView.getProgress() == 0f){
-   //         animator.setStartDelay(400);
-   //         animator.start();
-   //     }
-   //     else{
-   //         lottieAnimationView.setProgress(0f);
-   //     }
-   // }
+    public void openLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
